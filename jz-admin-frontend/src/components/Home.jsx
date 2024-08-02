5277406beb22abcf4ca740ec877005a48a06ee18
Juan Marcelo Rodriguez <juanmrodriguez@alumnos.exa.unicen.edu.ar>
2024-08-01 23:57:08 -0300

    chore: Update home page layout and styles

--- jz-admin-frontend/src/components/Home.jsx
+++ jz-admin-frontend/src/components/Home.jsx
@@ -1,27 +1,27 @@
 //crear la pagina home del jardin infantes con un header ,un main, un footer y un aside con un menu de navegaciony ademas un boton ingreso y un boton registrarse
 
-import React from 'react';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
-import Aside from './Aside';
-import Nav from './Nav';
-import Button from './Button';
-import '../styles/Home.css';
+import React from 'react'
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+import Aside from './Aside'
+import Nav from './Nav'
+import Button from './Button'
+import '../styles/Home.css'
 
 const Home = () => {
-    return (
-        <div className="container">
-            <Header />
-            <Aside>
-                <Nav />
-            </Aside>
-            <Main />
-            <Footer />
-            <Button text="Ingresar" />
-            <Button text="Registrarse" />
-        </div>
-    );
-    }
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Aside>
+          <Nav />
+        </Aside>
+        <Main />
+      </div>
+      <Footer />
+    </>
+  )
+}
 
-    export default Home;
+export default Home
